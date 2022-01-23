1499a7b59c82d877ffd1a14a2b3962755a8775b8
Syntax <Cavitt@users.noreply.github.com>
2022-01-23 17:25:45 -0600

    Tabs

--- plugins/figma/viewer/src/index.tsx
+++ plugins/figma/viewer/src/index.tsx
@@ -3,6 +3,7 @@ import 'figma-plugin-ds/dist/figma-plugin-ds.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Tabs from '@radix-ui/react-tabs';
 import Editor from '@monaco-editor/react';
 import {useEditor} from 'hooks/useEditor';
 import {useCode} from 'hooks/useCode';
@@ -11,7 +12,7 @@ import {Hint} from 'views/Hint';
 
 import * as config from 'config';
 
-function ComponentViewer() {
+function App() {
   const editor = useEditor();
   const code = useCode();
 
@@ -19,19 +20,34 @@ function ComponentViewer() {
   if (!code) return <Hint/>;
 
   return (
-    <Editor
-      value={code}
-      height="100vh"
-      path="Test.tsx"
-      className="code-editor"
-      defaultLanguage="typescript"
-      theme={config.code.editor.theme}
-      options={config.code.editor}
-    />
+    <Tabs.Root defaultValue="editor" orientation="vertical" className="tabs">
+      <Tabs.List aria-label="tools" className="tab-bar">
+        <Tabs.Trigger value="editor" className="tab-label">
+          Editor
+        </Tabs.Trigger>
+        <Tabs.Trigger value="preview" className="tab-label">
+          Preview
+        </Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="editor" className="tab">
+        <Editor
+          value={code}
+          height="100vh"
+          path="Test.tsx"
+          className="code-editor"
+          defaultLanguage="typescript"
+          theme={config.code.editor.theme}
+          options={config.code.editor}
+        />
+      </Tabs.Content>
+      <Tabs.Content value="preview" className="tab">
+        Coming soon...
+      </Tabs.Content>
+    </Tabs.Root>
   );
 }
 
 ReactDOM.render(
-  <ComponentViewer/>,
+  <App/>,
   document.getElementById('app'),
 );
