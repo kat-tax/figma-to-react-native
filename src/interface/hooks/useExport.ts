c9df831b13065b200a5b6366cebd0f17daf9e9ac
Syntax <2079305+TheUltDev@users.noreply.github.com>
2023-04-08 19:29:56 +0900

    Remove dead code

--- src/interface/hooks/useExport.ts
+++ src/interface/hooks/useExport.ts
@@ -6,7 +6,7 @@ export function useExport() {
     const onMessage = (e: MessageEvent) => {
       if (e.data?.pluginMessage?.type === 'compile') {
         const {project, files, theme} = e.data.pluginMessage;
-        saveFilesFallback(project, JSON.parse(files), theme);
+        saveFiles(project, JSON.parse(files), theme);
       }
     };
     addEventListener('message', onMessage);
@@ -14,7 +14,7 @@ export function useExport() {
   }, []);
 }
 
-async function saveFilesFallback(project: string, files: string[][], theme: string) {
+async function saveFiles(project: string, files: string[][], theme: string) {
   const lastModified = new Date();
   const payload: {name: string, lastModified: Date, input: string}[] = [];
   payload.push({name: 'theme.ts', lastModified, input: theme});
@@ -31,36 +31,3 @@ async function saveFilesFallback(project: string, files: string[][], theme: stri
   link.remove();
   setTimeout(() => URL.revokeObjectURL(source), 1000);
 }
-
-/*
-  async function saveFiles(files: string[][]) {
-    if (!('showDirectoryPicker' in window)) {
-      alert('Your browser does not support the Native File System API.');
-      return;
-    }
-
-    try {
-      const folder = await (window as any).showDirectoryPicker();
-      await Promise.all(
-        files
-          .filter(file => file.length === 0)
-          .map(([name, code]) =>
-            writeFile(folder, `${name}.tsx`, code)
-          )
-      );
-
-      alert('Files saved successfully.');
-    } catch (error) {
-      console.error('Error saving files:', error);
-      alert('Error saving files. Please try again.');
-    }
-  }
-
-  async function writeFile(directoryHandle: FileSystemDirectoryHandle, fileName: string, content: string) {
-    const fileHandle = await directoryHandle.getFileHandle(fileName, {create: true});
-    // @ts-ignore
-    const writableStream = await fileHandle.createWritable();
-    await writableStream.write(content);
-    await writableStream.close();
-  }
-*/
