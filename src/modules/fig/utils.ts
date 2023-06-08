2bf8c6397fcdd6eae88a3ea4de11f1ab8f218008
Syntax <2079305+TheUltDev@users.noreply.github.com>
2023-06-07 21:53:11 -0500

    Bug fixes

--- src/modules/fig/utils.ts
+++ src/modules/fig/utils.ts
@@ -281,26 +281,26 @@ export function getLetterSpacing(node: TargetNode): number | undefined {
 export function getFontWeight(style: string) {
   switch (style.replace(/\s*italic\s*/i, '')) {
     case 'Thin':
-      return 100;
+      return '100';
     case 'Extra Light':
     case 'Extra-light':
-      return 200;
+      return '200';
     case 'Light':
-      return 300;
+      return '300';
     case 'Regular':
-      return 400;
+      return '400';
     case 'Medium':
-      return 500;
+      return '500';
     case 'Semi Bold':
     case 'Semi-bold':
-      return 600;
+      return '600';
     case 'Bold':
-      return 700;
+      return '700';
     case 'Extra Bold':
     case 'Extra-bold':
-      return 800;
+      return '800';
     case 'Black':
-      return 900;
+      return '900';
   }
-  return 400;
+  return '400';
 }
