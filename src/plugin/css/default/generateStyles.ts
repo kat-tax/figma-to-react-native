71e21e528fe68ac329de4d42ef59ee4f7a0ccc22
Syntax <2079305+TheUltDev@users.noreply.github.com>
2023-10-04 03:50:01 -0500

    Refactors

--- src/plugin/css/default/generateStyles.ts
+++ src/plugin/css/default/generateStyles.ts
@@ -1,5 +1,4 @@
 import {convertToReactNative} from './lib/convertToReactNative';
-import {shorthandStyles} from './lib/shorthandStyles';
 import {processStyles} from './lib/processStyles';
 
 import type {ParseStyles} from 'types/parse';
@@ -7,5 +6,5 @@ import type {ParseStyles} from 'types/parse';
 export async function generateStyles(node: SceneNode): Promise<ParseStyles> {
   const css = await node.getCSSAsync();
   const styles = convertToReactNative(css);
-  return shorthandStyles(processStyles(styles));
+  return processStyles(styles);
 }
--- src/plugin/css/service/generateStyles.ts
+++ src/plugin/css/service/generateStyles.ts
@@ -1,29 +1,20 @@
 import type {ParseStyles} from 'types/parse';
 
+const STYLES_API = 'http://localhost:8000';
+
 export async function generateStyles(node: SceneNode): Promise<ParseStyles> {
-  // TODO: send preview image of node to assist AI in generating styles
-  // const img = await node.exportAsync({format: 'PNG'});
+  // Get CSS from node
   const css = await node.getCSSAsync();
 
-  // Build CSS file
-  let body = '.root { ';
-  for (const [k, v] of Object.entries(css)) {
-    body += `${k}: ${v}; `;
-  }
-  body += ' }';
-
   // Send CSS file to service
-  const endpoint = 'http://localhost:8000';
-  const output = await fetch(endpoint, {
-    body,
+  const output = await fetch(STYLES_API, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'text/css',
-    }
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(css),
   });
 
   // Parse response and return styles
-  const data = await output.json();
-  console.log(data);
-  return data.declarations.root.style;
+  const styles = await output.json();
+  console.log(css, styles);
+  return styles;
 }
