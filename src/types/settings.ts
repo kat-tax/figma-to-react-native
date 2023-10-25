48bb80a578488df7eac13b6dc41db673a12f4812
Syntax <2079305+TheUltDev@users.noreply.github.com>
2023-10-24 23:46:54 -0500

    Schema update

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -3,17 +3,35 @@ import type {editor, languages} from 'monaco-editor/esm/vs/editor/editor.api';
 import type {Options as CodeBlockWriterOptions} from 'code-block-writer';
 
 export interface Settings {
+  /**
+  * Options for code generation.
+  */
   react: {
+    /**
+     * The flavor of React Native to generate
+     */
     flavor: 'react-native' | 'tamagui',
+    /**
+     * The engine used to convert Figma styles to React Native
+     */
     styleGen:  'default' | 'service' | 'experimental',
+    /**
+     * Enable to add translations tags to strings
+     */
     addTranslate?: boolean,
   }
   writer: CodeBlockWriterOptions,
+  /**
+  * Options for the code editor.
+  */
   monaco: {
     general: editor.IStandaloneEditorConstructionOptions,
     compiler?: languages.typescript.CompilerOptions,
     inlayHints?: languages.typescript.InlayHintsOptions,
     diagnostics?: languages.typescript.DiagnosticsOptions,
   },
+  /**
+  * Options for the preview compiler.
+  */
   esbuild: TransformOptions,
 }
