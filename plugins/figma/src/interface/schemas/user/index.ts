115d95d6c8e3d1f364e91e3b418a70085bdda75e
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-01-07 15:34:12 -0600

    Remove user val

--- plugins/figma/src/interface/schemas/user/index.ts
+++
@@ -1,6 +0,0 @@
-import {JSONSchemaBridge} from 'uniforms-bridge-json-schema';
-
-import schema from './schema.json';
-import validator from './validator';
-
-export default new JSONSchemaBridge({schema, validator});
