c78527fbc2e05ec244f334eadba500df7ec3daa7
Syntax <2079305+TheUltDev@users.noreply.github.com>
2024-01-07 19:44:15 -0600

    Fix stylegen

--- plugins/figma/src/interface/hooks/useStyleGenServer.ts
+++ plugins/figma/src/interface/hooks/useStyleGenServer.ts
@@ -1,14 +1,12 @@
 import {useEffect} from 'react';
 import {on, emit} from '@create-figma-plugin/utilities';
-import {init, cssToReactNative, buildCSS} from 'css-to-rn';
-
-init('https://esm.sh/lightningcss-wasm@1.22.0/lightningcss_node.wasm');
+import {cssToReactNative, buildCSS} from 'css-to-rn';
 
 import type {EventStyleGenReq, EventStyleGenRes} from 'types/events';
 
 export function useStyleGenServer() {
   useEffect(() => on<EventStyleGenReq>('STYLE_GEN_REQ', async (css) => {
-    const stylesheet = cssToReactNative(buildCSS(css));
+    const stylesheet = await cssToReactNative(buildCSS(css));
     emit<EventStyleGenRes>('STYLE_GEN_RES', stylesheet);
   }), []);
 }
