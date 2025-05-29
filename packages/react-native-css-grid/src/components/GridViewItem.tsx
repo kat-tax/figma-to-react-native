dfd6a63aa6bd921d7dcf53364fe34fe3f6066e31
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-05-29 11:58:46 -0500

    moved grid component to exo-kit

--- packages/react-native-css-grid/src/components/GridViewItem.tsx
+++
@@ -1,9 +0,0 @@
-import {View} from 'react-native';
-import type {GridViewItemProps} from 'types';
-
-/**
- * A component that represents an individual grid item
- */
-export function GridViewItem(props: GridViewItemProps) {
-  return <View {...props}/>;
-}
--- packages/react-native-css-grid/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type * from './types';
-export {GridView} from './components/GridView';
-export {GridViewItem} from './components/GridViewItem';
