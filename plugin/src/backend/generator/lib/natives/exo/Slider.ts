b2e874e8973016ca651d2ed2bb9cfea67c0d1c09
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-09-08 18:52:06 -0500

    fix slider

--- plugin/src/backend/generator/lib/natives/exo/Slider.ts
+++ plugin/src/backend/generator/lib/natives/exo/Slider.ts
@@ -1,54 +1,42 @@
 import {getFillToken} from 'backend/parser/lib';
 
+interface SliderTokens {
+  fillRange: string,
+  fillTrack: string,
+  fillThumb: string,
+}
+
 export function Slider(component: ComponentNode) {
   const nodeRange = component.findOne(c => c.name === 'Range' && c.type === 'RECTANGLE') as RectangleNode;
   const nodeTrack = component.findOne(c => c.name === 'Track' && c.type === 'RECTANGLE') as RectangleNode;
   const nodeThumb = component.findOne(c => c.name === 'Thumb' && c.type === 'ELLIPSE') as EllipseNode;
-
-  return template({
+  return slider({
     fillRange: getFillToken(nodeRange),
     fillTrack: getFillToken(nodeTrack),
     fillThumb: getFillToken(nodeThumb),
-    importStyles: `import {useStyles} from 'react-native-unistyles';\n`,
   }).slice(1);
 }
 
-export const props = [
-  ['onChange', '(min: number, max?: number) => void'],
-  ['name?', 'string'],
-  ['value?', 'number'],
-  ['step?', 'number'],
-  ['minimumValue?', 'number'],
-  ['maximumValue?', 'number'],
-  ['trackColor?', 'string'],
-  ['rangeColor?', 'string'],
-  ['thumbColor?', 'string'],
-  ['testID?', 'string'],
-];
+export const slider = (_: SliderTokens) => `
+import {Slider as ExoSlider} from 'react-exo/slider';
+import {withUnistyles} from 'react-native-unistyles';
 
-export const template = (_: {
-  fillRange: string,
-  fillTrack: string,
-  fillThumb: string,
-  importStyles: string,
-}) => `
-import {Slider} from 'react-exo/slider';
-${_.importStyles}
 export interface SliderProps {
-  ${props.map(p => p.join(': ')).join(',\n  ')}
+  onChange: (min: number, max?: number) => void,
+  name?: string,
+  value?: number,
+  step?: number,
+  minimumValue?: number,
+  maximumValue?: number,
+  trackColor?: string,
+  rangeColor?: string,
+  thumbColor?: string,
+  testID?: string
 }
 
-function SliderThemed(props: SliderProps) {
-  const {theme} = useStyles();
-  return (
-    <Slider
-      rangeColor={props.rangeColor || ${_.fillRange}}
-      trackColor={props.trackColor || ${_.fillTrack}}
-      thumbColor={props.thumbColor || ${_.fillThumb}}
-      {...props}
-    />
-  );
-}
-
-export {SliderThemed as Slider};
+export const Slider = withUnistyles(ExoSlider, (theme) => ({
+  rangeColor: ${_.fillRange},
+  trackColor: ${_.fillTrack},
+  thumbColor: ${_.fillThumb},
+}));
 `;
