458019ec4cdd8ad15f8b505508df9f50e028fab2
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-09-11 00:25:33 -0500

    [types] react-exo

--- .types/react-exo/lib/types/index.d.ts
+++ .types/react-exo/lib/types/index.d.ts
@@ -1,35 +1,6 @@
-declare const _default: {};
-export default _default;
-
-export * from './src/assets/book/Book.export';
-export * from './src/assets/code/Code.export';
-export * from './src/assets/game/Game.export';
-export * from './src/assets/icon/Icon.export';
-export * from './src/assets/image/Image.export';
-export * from './src/assets/lottie/Lottie.export';
-export * from './src/assets/map/Map.export';
-export * from './src/assets/markdown/Markdown.export';
-export * from './src/assets/model/Model.export';
-export * from './src/assets/pdf/Pdf.export';
-export * from './src/assets/rive/Rive.export';
-export * from './src/assets/video/Video.export';
-
-export * from './src/layout/gesture/Gesture.export';
-export * from './src/layout/motion/Motion.export';
-export * from './src/layout/navigation/Navigation.export';
-export * from './src/layout/skeleton/Skeleton.export';
-
-export * from './src/services/device/Device.export';
-export * from './src/services/fs/Fs.export';
-export * from './src/services/kv/Kv.export';
-export * from './src/services/redux/Redux.export';
-export * from './src/services/toast/Toast.export';
-
-export * from './src/widgets/checkbox/Checkbox.export';
-export * from './src/widgets/picker/Picker.export';
-export * from './src/widgets/progress/Progress.export';
-export * from './src/widgets/radio/Radio.export';
-export * from './src/widgets/slider/Slider.export';
-export * from './src/widgets/switch/Switch.export';
-
-export * from './src/utilities';
+export * from './src/index'
+export {}
+import _default from './src/index'
+export default _default
+export * from './src/index'
+export {}
